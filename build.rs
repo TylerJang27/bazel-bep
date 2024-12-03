d29cdf13ca9803234e411610e0bf3f0bb4e6aa82
Tyler Jang <tyler@trunk.io>
2024-12-03 09:33:56 -0800

    first attempt with pbjson_build

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{env, io, path::PathBuf};
 
 fn main() -> io::Result<()> {
     let protos = std::fs::read_dir("proto")?
@@ -12,12 +12,27 @@ fn main() -> io::Result<()> {
         })
         .map(|a| a.map(|a| a.path().to_string_lossy().into_owned()))
         .collect::<Result<Vec<_>, _>>()?;
+
+
+    let descriptor_path = PathBuf::from(env::var("OUT_DIR").unwrap())
+    .join("proto_descriptor.bin");
+
     let compiler = tonic_build::configure();
     #[cfg(not(feature = "client"))]
     let compiler = compiler.build_client(false);
     #[cfg(not(feature = "server"))]
     let compiler = compiler.build_server(false);
-    compiler.compile(&protos, &["proto/"])?;
+
+    compiler.file_descriptor_set_path(&descriptor_path)
+    .compile_well_known_types(true)
+        // Override prost-types with pbjson-types
+        .extern_path(".google.protobuf", "::pbjson_types")
+        .compile(&protos, &["proto/"])?;
+
+    let descriptor_set = std::fs::read(descriptor_path)?;
+    pbjson_build::Builder::new()
+        .register_descriptors(&descriptor_set)?
+        .build(&[".google"])?;
 
     Ok(())
 }
